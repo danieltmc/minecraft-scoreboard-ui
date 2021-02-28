fb94f9bff154f6956535328ee983b6de01eb906a
danieltmc <me@danieltmc.com>
2021-02-28 10:34:36 -0500

    Add BedwarsStatsKeyEnum

---
+++ src/models/Enums.ts
@@ -0,0 +1,14 @@
+export enum BedwarsStatsKeyEnum {
+    beds_destroyed,
+    cache_rank,
+    deaths,
+    final_kills,
+    kd,
+    kills,
+    loses,
+    play_time,
+    rank,
+    rounds_played,
+    wins,
+    wl
+}
